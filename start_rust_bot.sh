12fe6a419a5ee446a87f37a1f595ad41a12c7215
Gil Fernandes <gil.fernandes@onepointltd.com>
2026-06-29 15:47:56 +0100

    Moved configuration file to right path

--- start_rust_bot.sh
+++ start_rust_bot.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 cargo build -r
-./target/release/rust-bot agent --config ./configs/openai-compat/config_gmail_anthropic.json --no-logs
+./target/release/rust-bot agent --config ./configs/anthropic/config_gmail_anthropic.json --no-logs
